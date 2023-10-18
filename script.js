4adc6d4db5ec768dbc19b7a4afb01b8eccf8ae8b
creyes25 <carolina.reyes2022@gmail.com>
2023-10-18 01:56:35 -0500

    add add feature to move box to the right or left

---
+++ script.js
@@ -0,0 +1,17 @@
+const clickBtn = document.querySelector('.click')
+const box = document.querySelector('.box')
+
+
+function moveBox () {
+  if (box.className.includes('right')) {
+    box.classList.remove('right')
+    box.classList.add('left')
+  }else {
+    box.classList.remove('left')
+    box.classList.add('right')
+  }
+
+}
+
+
+clickBtn.addEventListener('click', moveBox)
